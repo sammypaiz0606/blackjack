001e1f0a2d0ac2f1bbe663fce0eba69c3673b5b5
Your Name <youremail@yourdomain.com>
2022-07-27 21:34:51 -0700

    click response

--- js/javascript.js
+++ js/javascript.js
@@ -17,6 +17,9 @@ window.onload = function() {
 
 }
 
+
+
+
 function buildDeck() {
     let values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
     let types = ['C', 'D', 'H', 'S'];
